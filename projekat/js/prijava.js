acc3c99323e011703896bbe77ab4a4f2fe05e2f9
nikmasi <masanikolic002@gmail.com>
2024-06-06 01:11:54 +0200

    en_stranice, footer, ikonice

--- projekat/js/prijava.js
+++ projekat/js/prijava.js
@@ -24,7 +24,7 @@ function prijava(){
         if(korisnici[i].ime==unosime && korisnici[i].lozinka==unoslozinka){
 
             localStorage.setItem('trenutnikorisnik', unosime);
-            window.location.href="index2_ulogovan.html";
+            window.location.href="index_ulogovan.html";
             return;
         }
 
@@ -32,6 +32,34 @@ function prijava(){
     
 
     document.getElementById("greska").innerHTML="Pogrešno uneseno korisničko ime ili lozinka";
+    setTimeout(function(){
+        document.getElementById("greska").innerHTML=""
+        document.getElementById("unos_ime").value="";
+        document.getElementById("unos_lozinka").value="";
+    },2000)
+    
+
+}
+
+
+function prijava_en(){
+
+    let unosime=document.getElementById("unos_ime").value;
+    let unoslozinka=document.getElementById("unos_lozinka").value;
+
+
+    for(let i=0;i<korisnici.length;i++){
+        if(korisnici[i].ime==unosime && korisnici[i].lozinka==unoslozinka){
+
+            localStorage.setItem('trenutnikorisnik', unosime);
+            window.location.href="index_en_ulogovan.html";
+            return;
+        }
+
+    }
+    
+
+    document.getElementById("greska").innerHTML="Incorrect username or password entered";
     setTimeout(function(){
         document.getElementById("greska").innerHTML=""
         document.getElementById("unos_ime").value="";
